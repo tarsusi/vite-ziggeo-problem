ae0fb6257357484918d0d1a8c9d6d4d26d1514b9
tarsusi <omer.burak.karatas@outlook.com>
2022-09-20 12:19:21 +0300

    uses ziggeo latest sdk files

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue';
-import 'ziggeo-client-sdk/build/ziggeo';
+// import 'ziggeo-client-sdk/build/ziggeo';
+
+import './assets/ziggeo/ziggeo';
 
 let recorder;
 let params = {
